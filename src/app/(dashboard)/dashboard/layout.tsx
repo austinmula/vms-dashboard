024b5ebccbffd7683fa0611986b77138c2640743
austinmula <maustin64.ma@gmail.com>
2025-10-04 18:04:49 +0300

    Add "use client" directive to dashboard layout and pages for client-side rendering

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import { Layout, Menu } from "antd";
 
-export const metadata = {
-  title: "Dashboard | VMS",
-};
+// export const metadata = {
+//   title: "Dashboard | VMS",
+// };
 
 const { Header, Sider, Content } = Layout;
 
